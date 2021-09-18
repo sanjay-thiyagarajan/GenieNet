db7993150197d48e9bf78bd0ba54db8e3b0363af
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2021-09-18 20:16:52 +0530

    Fixing

--- predictor/urls.py
+++ predictor/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from predictor.views import *
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('verify/', verify, name='verify'),
+    path('results/', results, name='results')
+
 ]
